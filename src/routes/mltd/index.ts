e6af77f070c3eb6e57deea3fff331358cbf0573c
我可能很廢 <nicks96432@gmail.com>
2021-09-14 20:16:19 +0800

    update dependencies & remove project workflow (#5)

--- src/routes/mltd/index.ts
+++ src/routes/mltd/index.ts
@@ -1,9 +1,9 @@
 import { FastifyPluginCallback } from "fastify";
 import v1 from "./v1";
 
-const routes: FastifyPluginCallback = (instance, _opts, done) => {
+const Routes: FastifyPluginCallback = (instance, _opts, done) => {
     instance.register(v1, { prefix: "/v1" });
     done();
 };
 
-export default routes;
+export default Routes;
